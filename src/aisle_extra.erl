f904b972757addf53654f4dfc1c7d9985f289bc7
Robert Forbes <robert.forbes@pentlandedge.com>
2018-03-21 22:57:34 +0000

    Added an initial library skeleton.

---
+++ src/aisle_extra.erl
@@ -0,0 +1,18 @@
+-module(aisle_extra).
+
+%% aisle_extra: aisle_extra library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
